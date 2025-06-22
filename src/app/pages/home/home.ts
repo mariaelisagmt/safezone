96780180fc808f524034ba03c8cbd557e640324f
victor elpidio <victorelpidio950@gmail.com>
2025-06-22 14:40:19 -0300

    FEAT:  melhora do desing tabela

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Listocorrences } from '../../components/listocorrences/listocorrences';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Listocorrences],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
